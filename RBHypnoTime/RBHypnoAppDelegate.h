eaeb78dcd36490921ec43af373613276e57c4a37
Rashmi Bajaj <pszd8206@rbajaj.(none)>
2013-02-06 15:05:29 -0800

    Initial Commit

---
+++ RBHypnoTime/RBHypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RBHypnoAppDelegate.h
+//  RBHypnoTime
+//
+//  Created by Rashmi Bajaj on 2/6/13.
+//  Copyright (c) 2013 France Telecom Group (Orange) San Francisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RBHypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RBHypnoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RBHypnoTime
+//
+//  Created by Rashmi Bajaj on 2/6/13.
+//  Copyright (c) 2013 France Telecom Group (Orange) San Francisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RBHypnoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RBHypnoAppDelegate class]));
+    }
+}
